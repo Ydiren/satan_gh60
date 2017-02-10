71e952de38c8e5d51b21ace593cd745bf0e65b2d
Zeal Jagannatha <zealjagannatha@gmail.com>
2017-02-10 01:59:59 -0800

    Moved bindings from pok3r

---
+++ Makefile
@@ -0,0 +1,60 @@
+KEYMAP=zeal
+PLATFORM=linux
+
+firmware/${KEYMAP}.hex: keymaps/${KEYMAP}.c tmk tkg firmware
+	# define GH60_REV_CHN in config.h
+	[ "$(grep GH60_REV_CHN tmk/keyboard/gh60/config.h | wc -l)" -eq "2" ] || echo '#define GH60_REV_CHN' >> tmk/keyboard/gh60/config.h
+	# comment on unused features in Makefile
+	sed -i \
+		-e 's/^KEYMAP_IN_EEPROM_ENABLE/#&/' \
+		-e 's/^KEYMAP_SECTION_ENABLE/#&/' \
+		-e 's/^SOFTPWM_LED_ENABLE/#&/' \
+		-e 's/^LEDMAP_ENABLE/#&/' \
+		-e 's/^LEDMAP_IN_EEPROM_ENABLE/#&/' \
+		-e 's/^FADING_LED_ENABLE/#&/' \
+		-e 's/^BACKLIGHT_ENABLE/#&/' \
+		-e 's/^BREATHING_LED_ENABLE/#&/' \
+		tmk/keyboard/gh60/Makefile
+	# copy keymap into place
+	cp keymaps/${KEYMAP}.c tmk/keyboard/gh60/keymap_custom.c
+	# build the keymap firmware
+	make -C tmk/keyboard/gh60 KEYMAP=custom
+	# copy out the resulting firmware file
+	cp tmk/keyboard/gh60/gh60_lufa.hex $@
+
+install: firmware/${KEYMAP}.hex tkg/${PLATFORM}/conf/default.ini
+	# copy firmware into place
+	cp firmware/${KEYMAP}.hex tkg/common/firmware/gh60-revchn.hex
+	# run the reflash script (as root)
+	sudo tkg/${PLATFORM}/reflash.sh
+
+all: firmware/${KEYMAP}.hex install
+
+tkg/${PLATFORM}/conf/default.ini: tkg/${PLATFORM}/conf
+	echo 'Name="GH60 RevCHN"' >> $@
+	echo 'MCU=atmega32u4' >> $@
+	echo 'Firmware=gh60-revchn.hex' >> $@
+	echo 'Bootloader=atmel_dfu' >> $@
+
+tkg/${PLATFORM}/conf:
+	mkdir -p $@
+
+clean:
+	rm -rf firmware
+	rm -f tmk/keyboard/gh60/keymap_custom.c
+	rm -f tkg/common/firmware/gh60-revchn.hex
+	[ -d tmk/keyboard/gh60 ] && make -C tmk/keyboard/gh60 clean
+
+fullclean: clean
+	cd tmk ; git checkout -- .
+	cd tkg ; git checkout -- .
+
+firmware:
+	mkdir -p firmware
+
+tkg tmk: 
+	git submodule init
+	git submodule update
+	cd tmk ; git submodule init ; git submodule update
+
+.PHONY: install clean fullclean
---
+++ keymaps/zeal.c
@@ -0,0 +1,82 @@
+#include "keymap_common.h"
+
+const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /* 0: qwerty */
+    KEYMAP_ANSI(
+        GRV, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSPC, \
+        TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSLS, \
+        FN1, A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,     ENT,  \
+        LSFT,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,          RSFT, \
+        LCTL,LGUI,LALT,          SPC,                     RALT,FN0, FN0, RCTL),
+    /* 1: fn layer */
+    KEYMAP_ANSI(
+        ESC, F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12, DEL, \
+        TRNS,MPRV,MPLY,MNXT,TRNS,TRNS,TRNS,PGUP,HOME,PGDN,TRNS,TRNS,TRNS,TRNS,  \
+        TRNS,HOME,VOLD,VOLU,END, TRNS,LEFT,DOWN,UP,  RGHT,TRNS,TRNS,     TRNS, \
+        TRNS,MUTE,TRNS,FN2, FN3, TRNS,END, TRNS,PGUP,PGDN,DEL,           TRNS, \
+        TRNS,TRNS,TRNS,          ESC,                     TRNS,TRNS,TRNS,TRNS),
+    /* 2: unused */
+    KEYMAP_ANSI(
+        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, \
+        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, \
+        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,     TRNS, \
+        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,          TRNS,   \
+        TRNS,TRNS,TRNS,          TRNS,                    TRNS,TRNS,TRNS,TRNS),
+    /* 3: unused */
+    KEYMAP_ANSI(
+        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, \
+        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, \
+        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,     TRNS, \
+        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,          TRNS,   \
+        TRNS,TRNS,TRNS,          TRNS,                    TRNS,TRNS,TRNS,TRNS),
+    /* 4: unused */
+    KEYMAP_ANSI(
+        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, \
+        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, \
+        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,     TRNS, \
+        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,          TRNS,   \
+        TRNS,TRNS,TRNS,          TRNS,                    TRNS,TRNS,TRNS,TRNS),
+    /* 5: unused */
+    KEYMAP_ANSI(
+        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, \
+        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, \
+        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,     TRNS, \
+        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,          TRNS, \
+        TRNS,TRNS,TRNS,          TRNS,                    TRNS,TRNS,TRNS,TRNS),
+    /* 6: unused */
+    KEYMAP_ANSI(
+        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, \
+        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, \
+        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,     TRNS, \
+        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,          TRNS, \
+        TRNS,TRNS,TRNS,          TRNS,                    TRNS,TRNS,TRNS,TRNS),
+    /* 7: unused */
+    KEYMAP_ANSI(
+        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, \
+        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, \
+        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,     TRNS, \
+        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,          TRNS, \
+        TRNS,TRNS,TRNS,          TRNS,                    TRNS,TRNS,TRNS,TRNS),
+};
+const uint16_t PROGMEM fn_actions[] = {
+    /* Poker Layout */
+    [0] = ACTION_LAYER_MOMENTARY(1),  // to Fn overlay
+    [1] = ACTION_LAYER_TAP_KEY(1, KC_ESC), // tap for escape, hold for Fn
+    [2] = ACTION_MODS_KEY(MOD_LSFT | MOD_LCTL, KC_C),  // terminal copy
+    [3] = ACTION_MODS_KEY(MOD_LSFT | MOD_LCTL, KC_V),  // terminal paste
+    [4] = ACTION_DEFAULT_LAYER_SET(0),  // unused
+    [5] = ACTION_DEFAULT_LAYER_SET(0),  // unused
+    [6] = ACTION_DEFAULT_LAYER_SET(0),  // unused
+    [7] = ACTION_DEFAULT_LAYER_SET(0),  // unused
+    [8] = ACTION_DEFAULT_LAYER_SET(0),  // unused
+};
+
+#ifdef KEYMAP_IN_EEPROM_ENABLE
+uint16_t keys_count(void) {
+    return sizeof(keymaps) / sizeof(keymaps[0]) * MATRIX_ROWS * MATRIX_COLS;
+}
+
+uint16_t fn_actions_count(void) {
+    return sizeof(fn_actions) / sizeof(fn_actions[0]);
+}
+#endif
